69ae97ae7705237c3147a42661b7d75c1612e85c
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-06-25 01:59:14 +0000

    app completed

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const numberOfPeople = prompt("How many people?");
-const numberOfPizzas = prompt("How many pizzas do you have?");
-
-prompt = (`${numberOfPeople} people with ${numberOfPizzas} pizzas`);
-
 // Note: There are 8 slices in a pizza
 
-let slicesPerPerson = numberOfPizzas * 8 / numberOfPeople;
-console.log(slicesPerPerson);
+// const numberOfPeople = prompt("How many people?");
+// const numberOfPizzas = prompt("How many pizzas do you have?");
+// const slicesPerPerson = numberOfPizzas * 8 /  numberOfPeople;
+// const remainderOfSlices = numberOfPizzas * 8 %  numberOfPeople;
+
+let isNotNumeric = true;
+while (isNotNumeric) {
+  const numberOfPeople = prompt("How many people?");
+  const numberOfPizzas = prompt("How many pizzas do you have?");
+
+  const numOfPeople = parseInt(numberOfPeople);
+  const numOfPizzas = parseInt(numberOfPizzas);
+
+  if (!isNaN(numOfPeople) && !isNaN(numOfPizzas)) {
+    const slicesPerPerson = numberOfPizzas * 8 / numberOfPeople;
+    const remainderOfSlices = numberOfPizzas * 8 % numberOfPeople;
+    console.log(`${numOfPeople} people with ${numOfPizzas} pizzas`);
+    if (slicesPerPerson && remainderOfSlices > 1) {
+      console.log(`Each person gets ${slicesPerPerson} pieces of pizza`);
+      console.log(`There are ${Math.floor(remainderOfSlices)} leftover pieces`);
+    } else {
+      console.log(`Each person gets ${slicesPerPerson} piece of pizza`);
+      console.log(`There are ${remainderOfSlices} leftover piece`);
+    }
+
+
+
+
+    isNotNumeric = false;
+  }
+
+
+}
+
+
+
+
